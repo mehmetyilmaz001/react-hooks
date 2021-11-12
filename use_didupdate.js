154e0ac2dccab445b4731d4844e20a730b2a2c49
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2021-11-12 13:33:45 +0300

    initial

---
+++ use_didupdate.js
@@ -0,0 +1,14 @@
+export const  useDidUpdate = (callback, deps) => {
+    const hasMount = useRef(false)
+  
+    useEffect(() => {
+      if (hasMount.current) {
+        callback()
+      } else {
+        hasMount.current = true
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, deps)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }
